b5ba5ec66a9e03f7a603bf0782f2cc036553d562
Neeharika Baral <neehabaral72@gmail.com>
2024-02-08 02:25:29 -0500

    add scss styling to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,12 +4,12 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return (
-    <>
+    <div className ="photo-list__item">
       <img src={props.photo.imageSource} alt="Photo"/> <br/>
       <img src={props.photo.profile} alt="Profile"/> <br/>
       {props.photo.username} <br/>
       {props.photo.location.city}, {props.photo.location.country}
-    </>
+    </div>
   )
 };
 
