13baf939587b15fd2ad8c31ff17534d1205f0522
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 16:25:51 -0500

    change if statement in likesStoringFunction to ternary syntax. remove console.log

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,15 +7,10 @@ const HomeRoute = ({ topics, photos }) => {
   const [likesArray, setLikesArray] = useState([]);
   const likesStoringFunction = (photoId) => {
     setLikesArray((prev) => {
-      console.log(prev)
-      if (prev.includes(photoId)) {
-        // likesStoringFunction is called when like button is clicked. if id already exists in array, that means the photo was UNliked (as id was previously liked and added). Now id needs to be removed from array
-        // remove id using filter: return new array containing all items from prev that =/= photoId
-        return prev.filter(id => id !== photoId);
-      } else {
-        // If the ID is not in the array, add it and return the new array
-        return prev = [...prev, photoId];
-      }
+      // first condition: likesStoringFunction is called when like button is clicked. if id already exists in array, that means the photo was UNliked (as id was previously liked and added). Now id needs to be removed from array. remove id using filter: return new array containing all items from prev that =/= photoId
+      // second condition:  If the ID is not in the array, add it and return the new array
+      return prev.includes(photoId) ? prev.filter(id => id !== photoId) : prev = [...prev, photoId];
+
     })
   }
 
