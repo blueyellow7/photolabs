8c28ae644931e8aefe47bfe6622dc007f5b1f929
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 18:47:18 -0500

    move onClick event from closeSymbol img to the button enclosing the img

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,8 +5,8 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = ({ showModal, handleShowModal }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={handleShowModal}/>
+      <button className="photo-details-modal__close-button" onClick={handleShowModal}>
+        <img src={closeSymbol} alt="close symbol"/>
       </button>
     </div>
   )
