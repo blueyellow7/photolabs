57cde14e31c119143b370fdc22b8e3f44f2a6ce9
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 18:40:17 -0500

    pass showModal state and handleShowModal function to PhotoDetailsModal component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
 
-  // the handleShowModal function will be passed to an onClick listener for PhotoItemList image; when the photo is clicked, handleShowModal will change showModal state to 'true'
+  // the handleShowModal function will be passed to onClick listener for PhotoItemList image and [x] button on the Modal; when photo is clicked, handleShowModal will change showModal state to 'true'. when [x] is clicked it will change to 'false'
   const [showModal, setShowModal] = useState(false);
   const handleShowModal = () => { 
     setShowModal(prev => !prev)
@@ -19,7 +19,7 @@ const App = () => {
       <HomeRoute photos={photos} topics={topics} showModal={showModal} handleShowModal={handleShowModal} />
 
       {/* only if 'showModal' state = true, render PhotoDetailsModal */}
-      { showModal && <PhotoDetailsModal /> }
+      { showModal && <PhotoDetailsModal showModal={showModal} handleShowModal={handleShowModal}/> }
 
     </div>
   );
