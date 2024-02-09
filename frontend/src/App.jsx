92faad69926806c88eb7d2b65c653d71f280bb81
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 17:18:03 -0500

    add useState for PhotoDetailsModal component. create handleShowModal function for when a photo is clicked => will change state and render PhotoDetailsModal.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 
 const App = () => {
+
+  // the handleShowModal function will be passed to an onClick listener for PhotoItemList image; when the photo is clicked, handleShowModal will change showModal state to 'true'
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => { 
+    setShowModal(prev => !prev)
+  }
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} />
+      <HomeRoute photos={photos} topics={topics} showModal={showModal} handleShowModal={handleShowModal} />
+
+      {/* only if 'showModal' state = true, render PhotoDetailsModal */}
+      { showModal && <PhotoDetailsModal /> }
+
     </div>
   );
 };
