c27d472c37177a587004f5f15a471c6686aa4533
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 02:26:37 -0500

    define and pass specific values to PhotoListItem, instead of passing whole object

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -5,9 +5,16 @@ import PhotoListItem from "components/PhotoListItem.jsx";
 const PhotoList = ({ photos }) => {
   return (
     <ul className="photo-list">
-      {photos.map((photo) => {
-        return <li key={photo.id}><PhotoListItem photoData={photo} /></li>
-      } )}
+      {photos.map((photo) => { 
+        return <li key={photo.id}>
+        <PhotoListItem 
+          photoId={photo.id}
+          regularPhoto={photo.urls.regular}
+          profilePhoto={photo.user.profile}
+          username={photo.user.username}
+          location={photo.location}
+        />
+        </li>})}
     </ul>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,18 +3,21 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from 'components/PhotoFavButton';
 
 
-const PhotoListItem = (props) => {
+const PhotoListItem = ({ photoId, regularPhoto, profilePhoto, username, location }) => {
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
-      <img src={props.photoData.urls.regular} alt="Photo" className="photo-list__image"/>
+
+      <img src={regularPhoto} alt="Photo" className="photo-list__image"/>
+      
       <div className="photo-list__user-details">
-        <img src={props.photoData.user.profile} alt="Profile" className="photo-list__user-profile"/>
+        <img src={profilePhoto} alt="Profile" className="photo-list__user-profile"/>
         <div className="photo-list__user-info">
-          {props.photoData.user.username}
-          <p className="photo-list__user-location">{props.photoData.location.city}, {props.photoData.location.country}</p>
+          {username}
+          <p className="photo-list__user-location">{location.city}, {location.country}</p>
         </div>
       </div>
+      
     </div>
   )
 };
