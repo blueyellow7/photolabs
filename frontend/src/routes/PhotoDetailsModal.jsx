efc66a52fa033cb9a6a98f1fa46838e75d020cb9
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 23:38:42 -0500

    render full sized photo and user info in PhotoDetailsModal component

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = ({ showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
-  console.log("Display contents of selected photo:", selectedPhoto)
+const PhotoDetailsModal = ({ likesArray, likesStoringFunction, showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={handleShowModal}>
+      <button onClick={handleShowModal} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol"/>
       </button>
+
+      <img className="photo-details-modal__image" src={selectedPhoto.urls.full} alt="full photo"/>
+      
+      <div className="photo-details-modal__photographer-details">
+        <img src={selectedPhoto.user.profile} alt="Profile" className="photo-details-modal__photographer-profile"/>
+        <div className="photo-details-modal__photographer-info">
+          {selectedPhoto.user.name}
+          <p className="photo-details-modal__photographer-location">{selectedPhoto.location.city}, {selectedPhoto.location.country}</p>
+        </div>
+      </div>
+
     </div>
   )
 };
