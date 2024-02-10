461db880ddea3a4094f01985a37f0041864396e4
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 20:29:16 -0500

    add new handler function using useState to return object of a selected photo. pass state and handler down to HomeRoute and PhotoDetailsModal component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,36 @@ import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
 
-  // the handleShowModal function will be passed to onClick listener for PhotoItemList image and [x] button on the Modal; when photo is clicked, handleShowModal will change showModal state to 'true'. when [x] is clicked it will change to 'false'
+  // pass down handleShowModal to the onClick listener in the PhotoItemList image, and the PhotoDetailModal [x] button. when photo is clicked, handleShowModal will change showModal state to 'true'. when [x] is clicked it will change to 'false'
   const [showModal, setShowModal] = useState(false);
   const handleShowModal = () => { 
     setShowModal(prev => !prev)
   }
 
+  // pass down the handleSelectedPhoto function to the onClick listener in PhotoItemList. use the photoId data available in PhotoItemList as the arguement. function will change the selectedPhoto state to return the respective photo object.
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const handleSelectedPhoto = (selectedId) => {
+    setSelectedPhoto((prev) => {
+      prev = photos.find((photo) => photo.id === selectedId);
+      return prev;
+    })
+  }
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} showModal={showModal} handleShowModal={handleShowModal} />
+      <HomeRoute 
+        photos={photos}
+        topics={topics}
+        showModal={showModal} handleShowModal={handleShowModal}
+        selectedPhoto={selectedPhoto} handleSelectedPhoto={handleSelectedPhoto}
+      />
 
       {/* only if 'showModal' state = true, render PhotoDetailsModal */}
-      { showModal && <PhotoDetailsModal showModal={showModal} handleShowModal={handleShowModal}/> }
+      { showModal && <PhotoDetailsModal 
+                        showModal={showModal} handleShowModal={handleShowModal}
+                        selectedPhoto={selectedPhoto} handleSelectedPhoto={handleSelectedPhoto}
+                      /> 
+      }
 
     </div>
   );
