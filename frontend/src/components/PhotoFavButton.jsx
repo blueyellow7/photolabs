5a70c30f01a8bad16583b205b710b650a312e300
Neeharika Baral <neehabaral72@gmail.com>
2024-02-10 01:29:29 -0500

    remove 'like/setLike' state and set FavIcon to toggle like button based on 'likesArray' state instead

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,21 +4,17 @@ import '../styles/PhotoFavButton.scss';
 
 
 function PhotoFavButton({ photoId, likesArray, likesStoringFunction }) {
-  // Initialize 'like' state based on whether 'photoId' is in likesArray
-  const [like, setLike] = useState(likesArray.includes(photoId));
-  const handleLike = () => {
-    setLike((prevlike) => !prevlike)
-  };
-
   return (
     <div className="photo-list__fav-icon" 
-      onClick={() => { 
-        handleLike()
+      onClick={() => {
+        {/* likesStoringFunction adds and removes photoId from likesArray when like button is clicked */} 
         likesStoringFunction(photoId)
       }}
     >
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={like}/>
+        {/* toggle FavIcon colour based on likesArray state (ie: check if photoId is in array or not. 
+          not in array = selected = false, in array = selected = true) */}
+        <FavIcon selected={likesArray.includes(photoId)}/>
       </div>
     </div>
   );
