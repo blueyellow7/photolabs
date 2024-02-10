e9c94c8816232b4ed005c905807653b7332b0b2a
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 23:41:40 -0500

    render PhotoList component inside of PhotoDetailsModal to render array of similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,6 +4,11 @@ import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = ({ likesArray, likesStoringFunction, showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
+
+  // turn similar_photos object within selectPhoto object into an array 
+  const similarPhotosArray = Object.values(selectedPhoto.similar_photos);
+  console.log('similarPhotosArray:', similarPhotosArray)
+
   return (
     <div className="photo-details-modal">
       <button onClick={handleShowModal} className="photo-details-modal__close-button">
@@ -20,6 +25,13 @@ const PhotoDetailsModal = ({ likesArray, likesStoringFunction, showModal, handle
         </div>
       </div>
 
+      <p className="photo-details-modal__header">Similar Photos</p>
+      <PhotoList 
+        photos={similarPhotosArray}
+        likesArray={likesArray} likesStoringFunction={likesStoringFunction}
+        showModal={showModal} handleShowModal={handleShowModal}
+        selectedPhoto={selectedPhoto} handleSelectedPhoto={handleSelectedPhoto}
+      />
     </div>
   )
 };
