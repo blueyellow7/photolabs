1df0b96eee458b3964dddd8ec0cf8392b66648d8
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 20:34:50 -0500

    pass selectedPhoto state and handler function to PhotoDetailsModal component. check with console.log that selectedPhoto state is updating accordingly

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ showModal, handleShowModal }) => {
+const PhotoDetailsModal = ({ showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
+  console.log("Display contents of selected photo:", selectedPhoto)
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleShowModal}>
