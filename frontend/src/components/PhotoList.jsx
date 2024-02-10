846a3e4dc02989aa9db86a5b5b854960410c8407
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 23:31:09 -0500

    change username to name when rendering PhotoList

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,22 +3,22 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "components/PhotoListItem.jsx";
 
 const PhotoList = ({ photos, likesArray, likesStoringFunction, showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
-  return (
-    <ul className="photo-list">
+  return ( 
+      <ul className="photo-list">
       {photos.map((photo) => { 
         return <li key={photo.id}>
         <PhotoListItem 
           photoId={photo.id}
           regularPhoto={photo.urls.regular}
           profilePhoto={photo.user.profile}
-          username={photo.user.username}
+          name={photo.user.name}
           location={photo.location}
           likesArray={likesArray} likesStoringFunction={likesStoringFunction}
           showModal={showModal} handleShowModal={handleShowModal}
           selectedPhoto={selectedPhoto} handleSelectedPhoto={handleSelectedPhoto}
         />
         </li>})}
-    </ul>
+      </ul>
   );
 };
 
