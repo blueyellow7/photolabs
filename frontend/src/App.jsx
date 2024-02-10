c7a565bb089adac9ee8812231936cb56838e1d0b
Neeharika Baral <neehabaral72@gmail.com>
2024-02-10 03:35:46 -0500

    move and organize all state logic into useApplicationData.js (separation of concerns)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,15 @@
 import React, { useState } from 'react';
 import './App.scss';
-import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import useApplicationData from 'hooks/useApplicationData';
 
 
 const App = () => {
-  
-  const [likesArray, setLikesArray] = useState([]);
-  const handleLikesArray = (photoId) => {
-    setLikesArray((prev) => {
-      // first condition: handleLikesArray is called when like button is clicked. if id already exists in array, that means the photo was UNliked (as id was previously liked and added). Now id needs to be removed from array. remove id using filter: return new array containing all items from prev that =/= photoId
-      // second condition:  If the ID is not in the array, add it and return the new array
-      return prev.includes(photoId) ? prev.filter(id => id !== photoId) : prev = [...prev, photoId];
-
-    })
-  }
-
-  // pass down handleModalView to the onClick listener in the PhotoItemList image, and the PhotoDetailModal [x] button. when photo is clicked, handleModalView will change ModalView state to 'true'. when [x] is clicked it will change to 'false'
-  const [modalView, setModalView] = useState(false);
-  const handleModalView = () => { 
-    setModalView(prev => !prev)
-  }
-
-  // pass down the handleSelectedPhoto function to the onClick listener in PhotoItemList. use the photoId data available in PhotoItemList as the arguement. function will change the selectedPhoto state to return the respective photo object.
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const handleSelectedPhoto = (selectedId) => {
-    setSelectedPhoto((prev) => {
-      prev = photos.find((photo) => photo.id === selectedId);
-      return prev;
-    })
-  }
+  /* custom hook that handles our whole app's state */
+  const { state:{ likesArray, modalView, selectedPhoto }, handleLikesArray, handleModalView, handleSelectedPhoto } = useApplicationData();
 
   return (
     <div className="App">
---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import photos from "../mocks/photos";
+
+const useApplicationData = () => {
+
+  const [likesArray, setLikesArray] = useState([]);
+  const [modalView, setModalView] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  // function triggered by PhotoFavButton's onClick event:
+  // if likesArray already has photo id related to clicked <3 button, remove id from array -> else, update likesArray with new id
+  const handleLikesArray = (photoId) => {
+    setLikesArray((prev) => {
+      return prev.includes(photoId) ? prev.filter(id => id !== photoId) : prev = [...prev, photoId];
+    })
+  }
+  
+  // function triggered by onClick event on PhotoListItem img + onClick event on modal close button:
+  // change to opposite (truthy<->falsey) from prev state (initial=false) to toggle opening/closing of modal
+  const handleModalView = () => { 
+    setModalView(prev => !prev)
+
+  }
+
+  // function triggered by onClick event in PhotoListItem:
+  // finds and returns photo data of clicked on img using photo id as arguement
+  const handleSelectedPhoto = (selectedId) => {
+    setSelectedPhoto((prev) => {
+      prev = photos.find((photo) => photo.id === selectedId);
+      return prev;
+    })
+  }
+
+  return { 
+    state: {
+      likesArray,
+      modalView,
+      selectedPhoto
+    },
+    handleLikesArray,
+    handleModalView,
+    handleSelectedPhoto
+  };
+}
+
+export default useApplicationData;
