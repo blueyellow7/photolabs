f0a4d3273f550ad96a9122fd90e511c4c3e245dd
Neeharika Baral <neehabaral72@gmail.com>
2024-02-09 23:32:30 -0500

    lift likesArray state from HomeRoute.jsx to App.jsx

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,24 +3,14 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
-const HomeRoute = ({ topics, photos, showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
-  const [likesArray, setLikesArray] = useState([]);
-  const likesStoringFunction = (photoId) => {
-    setLikesArray((prev) => {
-      // first condition: likesStoringFunction is called when like button is clicked. if id already exists in array, that means the photo was UNliked (as id was previously liked and added). Now id needs to be removed from array. remove id using filter: return new array containing all items from prev that =/= photoId
-      // second condition:  If the ID is not in the array, add it and return the new array
-      return prev.includes(photoId) ? prev.filter(id => id !== photoId) : prev = [...prev, photoId];
-
-    })
-  }
+const HomeRoute = ({ topics, photos, likesArray, likesStoringFunction, showModal, handleShowModal, selectedPhoto, handleSelectedPhoto }) => {
 
   return (
     <div className="home-route">
       <TopNavigation topics={topics} likesArray={likesArray} />
       <PhotoList 
         photos={photos}
-        likesArray={likesArray}
-        likesStoringFunction={likesStoringFunction}
+        likesArray={likesArray} likesStoringFunction={likesStoringFunction}
         showModal={showModal} handleShowModal={handleShowModal}
         selectedPhoto={selectedPhoto} handleSelectedPhoto={handleSelectedPhoto}
       />
