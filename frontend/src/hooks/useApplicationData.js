e96996254ef491c6cadd463ed4dd4c86f94dbc9e
Neeharika Baral <neehabaral72@gmail.com>
2024-02-14 20:29:01 -0500

    refractor useApplicationData.js

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -2,25 +2,32 @@ import { useReducer } from 'react';
 import photos from "../mocks/photos";
 
 const useApplicationData = () => {
-  
+
   const initialState = {
     likesArray: [],
     modalView: false,
     selectedPhoto: null
   }
 
+  const ACTIONS = {
+    UPDATE_LIKES_ARRAY: 'UPDATE_LIKES_ARRAY',
+    TOGGLE_MODAL_VIEW: 'TOGGLE_MODAL_VIEW',
+    LOAD_SELECTED_PHOTO: 'LOAD_SELECTED_PHOTO'
+  }
+  
   function reducer(state, action) {
     switch (action.type) {
-      case "updateLikesArray":
+      case ACTIONS.UPDATE_LIKES_ARRAY:
         return { ...state, likesArray: action.updatedState };
 
-      case "toggleModalView":
+      case ACTIONS.TOGGLE_MODAL_VIEW:
         return { ...state, modalView: action.updatedState };
 
-      case "loadSelectedPhoto":
+      case ACTIONS.LOAD_SELECTED_PHOTO:
         return { ...state, selectedPhoto: action.updatedState };
         
-      default: throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
+      default:
+        throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
     }
   }
 
@@ -32,21 +39,21 @@ const useApplicationData = () => {
   const handleLikesArray = (photoId) => {
     const likesArray = state.likesArray;
     const updateLikesArray = likesArray.includes(photoId) ? likesArray.filter(id => id !== photoId) : [...likesArray, photoId];
-    dispatch({ type: "updateLikesArray", updatedState: updateLikesArray });
+    dispatch({ type: ACTIONS.UPDATE_LIKES_ARRAY, updatedState: updateLikesArray });
   }
 
   /* 2) function triggered by onClick event on PhotoListItem img + onClick event on modal close button:
     change to opposite (truthy<->falsey) from prev state (initial=false) to toggle opening/closing of modal */
   const handleModalView = () => {
     const toggleModalView = !state.modalView
-    dispatch({ type: "toggleModalView", updatedState: toggleModalView });
+    dispatch({ type: ACTIONS.TOGGLE_MODAL_VIEW, updatedState: toggleModalView });
   }
 
   /* 3) function triggered by onClick event in PhotoListItem:
     finds and returns photo data of clicked on img using photo id as arguement. data used to load modal */
   const handleSelectedPhoto = (selectedId) => {
     const loadSelectedPhoto = photos.find((photo) => photo.id === selectedId);
-    dispatch({ type: "loadSelectedPhoto", updatedState: loadSelectedPhoto });
+    dispatch({ type: ACTIONS.LOAD_SELECTED_PHOTO, updatedState: loadSelectedPhoto });
   }
 
   return {
