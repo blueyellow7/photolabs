f49bcf4670fe1a0f281e148ed16e001dcccd847c
Neeharika Baral <neehabaral72@gmail.com>
2024-02-15 15:03:09 -0500

    call handleAllLikedPhotos with onClick event in FavBadge. ensure that the function is only called if likesArray contains at least 1 photo id

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ likesArray, selected }) => {
+const FavBadge = ({ likesArray, selected, handleAllLikedPhotos }) => {
   return (
-    <div className='fav-badge'>
+    <div className='fav-badge' 
+        onClick={() => {
+          if(likesArray.length > 0) { 
+            handleAllLikedPhotos(likesArray) 
+          }
+        }}>
       <FavIcon displayAlert={!!likesArray.length} selected={selected}/>
     </div>
   ); 
